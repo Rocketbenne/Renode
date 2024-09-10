065b4ea7f677e9e827e50ea05574a7f835443e95
Benedikt Unterthurner <benedikt.unterthurner@alpitronic.it>
2024-09-10 13:00:02 +0200

    Initial commit

---
+++ shell_script/stm32f4.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+NAME="renode2"
+
+# get path to this directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Script directory: $SCRIPT_DIR"
+
+# Give execution rigths to this file
+chmod 755 $SCRIPT_DIR/../shell_script/stm32f4.sh
+
+# Start Docker Image
+docker run -it --rm --network=host -p 3333:3333 --name $NAME -v $SCRIPT_DIR/..:/stm32f4 antmicro/renode bash -c "renode --console -e 'include @/stm32f4/resc/stm32f4.resc'"
